9b4bb902509111f06c4e7b69954f649e3585b9f5
kamangir <arash@kamangir.net>
2022-06-12 16:16:08 -0400

    getting the GPUs to work - kamangir/kamangir#681

--- bolt2/utils.py
+++ bolt2/utils.py
@@ -3,9 +3,13 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def validate():
     import tensorflow as tf
 
-    logger.info("{} GPUs available.".format(len(
-        tf.config.list_physical_devices("GPU"))))
+    logger.info(
+        "{} GPUs available.".format(len(tf.config.list_physical_devices("GPU")))
+    )
+    logger.info("TensorFlow: {}".format(tf.__version__))
+
     return True
